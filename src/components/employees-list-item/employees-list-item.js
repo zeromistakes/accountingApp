306967bcaa7d3a8dcbcadc5f1860cfb2cf6b7c3a
Roman Voronin <zeromistakes4@gmail.com>
2022-06-29 16:57:02 +0300

    practice with class component / added state

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,28 +1,58 @@
 import './employees-list-item.css';
+import {Component} from "react";
 
-const EmployeesListItem = ({name, salary, isForSalaryRaise}) => {
-    let listClassName = "list-group-item d-flex justify-content-between"
-    if(isForSalaryRaise) {
-        listClassName += ' increase';
+class EmployeesListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isForSalaryRaise: false,
+            isFavourite: false,
+        }
     }
-    return (
-        <li className={listClassName}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={`${salary}$`}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                        className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"/>
-                </button>
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"/>
-                </button>
-                <i className="fas fa-star"/>
-            </div>
-        </li>
-    )
+    toggleCookie = () => {
+        this.setState(({isForSalaryRaise}) => ({
+            isForSalaryRaise: !isForSalaryRaise,
+        }))
+    }
+
+    makeFavourite = () => {
+        this.setState(({isFavourite}) => ({
+            isFavourite: !isFavourite,
+        }))
+    }
+
+    render () {
+        const {name, salary} = this.props;
+        const {isForSalaryRaise, isFavourite} = this.state;
+        let listClassName = "list-group-item d-flex justify-content-between"
+        if(isForSalaryRaise) {
+            listClassName += ' increase';
+        }
+        if(isFavourite) {
+            listClassName += ' like';
+        }
+        return (
+            <li className={listClassName}>
+                <span onClick={this.makeFavourite} className="list-group-item-label">{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={`${salary}$`}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                            onClick={this.toggleCookie}
+                            className="btn-cookie btn-sm ">
+                        <i className="fas fa-cookie"/>
+                    </button>
+
+                    <button type="button"
+                            className="btn-trash btn-sm ">
+                        <i className="fas fa-trash"/>
+                    </button>
+                    <i className="fas fa-star"/>
+                </div>
+            </li>
+        )
+    }
+
 }
 
 export default EmployeesListItem;
